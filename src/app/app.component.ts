f7d2cf174b3e022417ceda70027b58651a747497
silverlovesl <541075770@qq.com>
2016-12-16 00:35:53 +0900

    First-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import {AccordionModule} from 'primeng/primeng';     //accordion and accordion tab
+import {MenuItem} from 'primeng/primeng';            //api
+import {ButtonModule} from 'primeng/primeng';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
